ebd77d79967cb3cb746c81d65b5d1301f48a0135
Djkde01 <sergio.estrella@utp.edu.co>
2023-10-30 16:38:10 -0500

    feat (Sort posts method)

--- src/js/utils.js
+++ src/js/utils.js
@@ -9,3 +9,48 @@ export function formatDate(date) {
     year: "numeric",
   });
 }
+
+export function formatBlogPosts(
+  posts,
+  {
+    filterOutDrafts = true,
+    filterFuturePosts = true,
+    sortBy = "date",
+    limit = undefined,
+  } = {}
+) {
+  const filteredPosts = posts.reduce((acc, post) => {
+    const { date, draft } = post.frontmatter;
+
+    if (filterOutDrafts && draft) {
+      return acc;
+    }
+    if (filterFuturePosts && new Date(date) > new Date()) {
+      return acc;
+    }
+    return [...acc, post];
+  }, []);
+
+  const sortedPosts = sortPosts(sortBy, filteredPosts);
+
+  return limit ? sortedPosts.slice(0, limit) : sortedPosts;
+}
+
+function sortPosts(sortType, posts) {
+  switch (sortType) {
+    case "date":
+      return posts.sort((a, b) => {
+        const dateA = new Date(a.frontmatter.date);
+        const dateB = new Date(b.frontmatter.date);
+        return dateB - dateA;
+      });
+    case "title":
+      return posts.sort((a, b) => {
+        const titleA = a.frontmatter.title;
+        const titleB = b.frontmatter.title;
+        return titleA.localeCompare(titleB);
+      });
+    default:
+      return posts;
+  }
+}
